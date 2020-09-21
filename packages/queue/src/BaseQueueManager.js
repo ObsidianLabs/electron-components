dbc3261962507a2f5f60fffde3677a088dfb7def
Phil <qft.gtr@gmail.com>
2020-09-21 14:20:37 +0800

    Update BaseQueueManager.js

--- packages/queue/src/BaseQueueManager.js
+++ packages/queue/src/BaseQueueManager.js
@@ -6,28 +6,23 @@ export default class BaseQueueManager {
   static button = null
   static pending = []
 
-  addToQueue (item) {
-    BaseQueueManager.pending.unshift(item)
-    BaseQueueManager.button.forceUpdate()
-  }
-
-  updateStatus (txHash, status, extraData, callbacks) {
+  updateStatus (txHash, status, data, callbacks) {
     const index = BaseQueueManager.pending.findIndex(item => item.txHash === txHash)
-    if (index === -1) {
-      return
-    }
 
     let tx
-    if (status !== 'CONFIRMED' && status !== 'FAILED') {
+    if (index === -1) {
+      tx = { txHash, status, ts: moment().unix(), data }
+      BaseQueueManager.pending.unshift(tx)
+    } else if (status !== 'CONFIRMED' && status !== 'FAILED') {
       tx = BaseQueueManager.pending[index]
       tx.status = status
       tx.ts = moment().unix()
-      tx.data = { ...tx.data, ...extraData }
+      tx.data = { ...tx.data, ...data }
     } else {
       tx = BaseQueueManager.pending.splice(index, 1)[0]
       tx.status = status
       tx.ts = moment().unix()
-      tx.data = { ...tx.data, ...extraData }
+      tx.data = { ...tx.data, ...data }
 
       const { network } = redux.getState()
       redux.dispatch('ADD_TRANSACTION', { network, tx })
