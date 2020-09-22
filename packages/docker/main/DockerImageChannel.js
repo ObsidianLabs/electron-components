ff1c5d4bca39ca6a728f1547c35400e812022f3e
Phil <qft.gtr@gmail.com>
2020-09-22 12:57:35 +0800

    set default filter & sort in DockerImageChannel

--- packages/docker/main/DockerImageChannel.js
+++ packages/docker/main/DockerImageChannel.js
@@ -1,10 +1,22 @@
 const { IpcChannel } = require('@obsidians/ipc')
+const semverLt = require('semver/functions/lt')
+const semverValid = require('semver/functions/valid')
+
+const defaultFilter = v => semverValid(v)
+const defaultSort = (v1, v2) => semverLt(v1, v2) ? 1 : -1
+
+const getHandler = (opt, defaultHandler) => {
+  if (typeof opt === 'boolean') {
+    return opt ? defaultHandler : undefined
+  }
+  return opt || defaultHandler
+}
 
 class DockerImageChannel extends IpcChannel {
   constructor (imageName, opts = {}) {
     super('docker-image', imageName)
-    this.filter = opts.filter || (() => true)
-    this.sort = opts.sort
+    this.filter = getHandler(opts.filter, defaultFilter)
+    this.sort = getHandler(opts.sort, defaultSort)
   }
 
   get imageName () {
@@ -16,7 +28,10 @@ class DockerImageChannel extends IpcChannel {
     let versions = logs.split('\n')
       .filter(Boolean)
       .map(JSON.parse)
-      .filter(({ Tag }) => this.filter(Tag))
+    
+    if (this.filter) {
+      versions = versions.filter(({ Tag }) => this.filter(Tag))
+    }
     if (this.sort) {
       versions = versions.sort((x, y) => this.sort(x.Tag, y.Tag))
     }
@@ -29,7 +44,10 @@ class DockerImageChannel extends IpcChannel {
 
   async remoteVersions (size = 10) {
     const res = await this.fetch(`http://registry.hub.docker.com/v1/repositories/${this.imageName}/tags`)
-    let versions = JSON.parse(res).filter(({ name }) => this.filter(name))
+    let versions = JSON.parse(res)
+    if (this.filter) {
+      versions = versions.filter(({ name }) => this.filter(name))
+    }
     if (this.sort) {
       versions = versions.sort((x, y) => this.sort(x.name, y.name))
     }
