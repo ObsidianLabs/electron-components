eb5c99e8f11715f1d154376ce3f2425994b44490
Leon <leon390422914@gmail.com>
2020-10-20 14:58:10 +0800

    aws

--- packages/auth/src/Auth.js
+++ packages/auth/src/Auth.js
@@ -1,5 +1,6 @@
 import redux from '@obsidians/redux'
 import decode from 'jwt-decode'
+import AWS from 'aws-sdk'
 
 import providers from './providers'
 
@@ -27,6 +28,7 @@ export default {
     const provider = query.get('provider')
 
     let token
+    let awsToken
     try {
       const response = await fetch(`${authServerUrl}/api/v1/auth/login`, {
         headers: {
@@ -38,10 +40,110 @@ export default {
       });
       const result = await response.json()
       token = result.token
+      awsToken = result.awsToken
     } catch (err) {
       history.replace('/')
       return
     }
+
+    const credential = await new Promise(resolve => {
+      const sts = new AWS.STS();
+      const params = {
+        WebIdentityToken: awsToken,
+        RoleArn: 'arn:aws:iam::023286913450:role/Cognito_webIDE_auth_testAuth_Role',
+        RoleSessionName: 'leontest',
+        DurationSeconds: 43200,
+      };
+      sts.assumeRoleWithWebIdentity(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else resolve(data); // successful response
+      });
+    });
+
+    console.log('credential', credential);
+
+    const accessKeyId = credential.Credentials.AccessKeyId;
+    const secretAccessKey = credential.Credentials.SecretAccessKey;
+    const sessionToken = credential.Credentials.SessionToken;
+
+    AWS.config.update({
+      accessKeyId,
+      secretAccessKey,
+      sessionToken,
+    });
+
+
+    const s3 = new AWS.S3();
+
+    // List
+    const s3ListResponse = await new Promise(resolve => {
+      const params = {
+        Bucket: 'webidetest',
+        Prefix: 'public/',
+      };
+      s3.listObjectsV2(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else resolve(data); // successful response
+      });
+    });
+    console.log('s3ListResponse', s3ListResponse);
+
+
+    // Create
+    const s3PutResponse = await new Promise(resolve => {
+      const params = {
+        Bucket: 'webidetest',
+        Key: 'public/test.txt',
+        Body: 'test string from putObject'
+      };
+      s3.putObject(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else resolve(data); // successful response
+      });
+    })
+    console.log('s3PutResponse', s3PutResponse)
+
+
+    // Read
+    const s3GetResponse = await new Promise(resolve => {
+      const params = {
+        Bucket: 'webidetest',
+        Key: 'public/test.txt',
+      };
+      s3.getObject(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else resolve(data); // successful response
+      });
+    });
+    console.log('s3GetResponse', s3GetResponse);
+
+
+    // Delete
+    const s3DeleteResponse = await new Promise(resolve => {
+      const params = {
+        Bucket: 'webidetest',
+        Key: 'public/test.txt',
+      };
+      s3.deleteObject(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else resolve(data); // successful response
+      });
+    })
+    console.log('s3DeleteResponse', s3DeleteResponse)
+
+
+    // List
+    const s3ListResponse2 = await new Promise(resolve => {
+      const params = {
+        Bucket: 'webidetest',
+        Prefix: 'public/',
+      };
+      s3.listObjectsV2(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else resolve(data); // successful response
+      });
+    });
+    console.log('s3ListResponse2', s3ListResponse2);
 
     if (!token) {
       history.replace('/')
