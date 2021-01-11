982c7579152c70e77961ce4682f0d3207f706481
Leon <7112003+leonlianght@users.noreply.github.com>
2021-01-11 18:32:17 +0800

    feat: add ddevtools toggle (#32)
    * feat: add ddevtools toggle
    
    * feat: check platform before handleClick

--- packages/global/src/modals/AboutModal.js
+++ packages/global/src/modals/AboutModal.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react'
 
 import { Modal } from '@obsidians/ui-components'
 import fileOps from '@obsidians/file-ops'
+import platform from '@obsidians/platform'
 
 import globalModalManager from './globalModalManager'
 
@@ -9,6 +10,9 @@ export default class AboutModal extends PureComponent {
   constructor (props) {
     super(props)
     this.modal = React.createRef()
+    this.state = {
+      debugCounter: 0
+    }
   }
 
   componentDidMount () {
@@ -19,6 +23,21 @@ export default class AboutModal extends PureComponent {
     this.modal.current.openModal()
   }
 
+  handleClick() {
+    if (platform.isWeb) {
+      return
+    }
+    if (this.state.debugCounter > 20) {
+      try {
+        console.info('Dev Tools Toggled')
+        window.require('electron').remote.getCurrentWindow().toggleDevTools()
+      } catch (e) {}
+      this.setState({ debugCounter: 0 })
+    } else {
+      this.setState({ debugCounter: this.state.debugCounter + 1 })
+    }
+  }
+
   render () {
     return (
       <Modal
@@ -27,7 +46,7 @@ export default class AboutModal extends PureComponent {
         textCancel='Close'
       >
         <div className='d-flex flex-column align-items-center justify-content-center'>
-          <img src={this.props.icon} style={{ background: 'transparent', width: '100px' }} />
+          <img src={this.props.icon} style={{ background: 'transparent', width: '100px' }} onClick={() => this.handleClick()}/>
           <p className='mt-3'><span className='h4'><b>{process.env.PROJECT_NAME}</b></span> v{fileOps.current.getAppVersion()}</p>
 
           <h5 className='small-caps mt-4'>contact us</h5>
