659d008cead117693fb66bdc858a6419fff00934
Leon <leon390422914@gmail.com>
2021-01-11 17:55:14 +0800

    feat: add ddevtools toggle

--- packages/global/src/modals/AboutModal.js
+++ packages/global/src/modals/AboutModal.js
@@ -9,6 +9,9 @@ export default class AboutModal extends PureComponent {
   constructor (props) {
     super(props)
     this.modal = React.createRef()
+    this.state = {
+      debugCounter: 0
+    }
   }
 
   componentDidMount () {
@@ -19,6 +22,18 @@ export default class AboutModal extends PureComponent {
     this.modal.current.openModal()
   }
 
+  handleClick() {
+    if (this.state.debugCounter > 20) {
+      try {
+        console.info('Dev Tools Toggled')
+        window.require('electron').remote.getCurrentWindow().toggleDevTools()
+      } catch (e) {}
+      this.setState({ debugCounter: 0 })
+    } else {
+      this.setState({ debugCounter: this.state.debugCounter + 1 })
+    }
+  }
+
   render () {
     return (
       <Modal
@@ -27,7 +42,7 @@ export default class AboutModal extends PureComponent {
         textCancel='Close'
       >
         <div className='d-flex flex-column align-items-center justify-content-center'>
-          <img src={this.props.icon} style={{ background: 'transparent', width: '100px' }} />
+          <img src={this.props.icon} style={{ background: 'transparent', width: '100px' }} onClick={() => this.handleClick()}/>
           <p className='mt-3'><span className='h4'><b>{process.env.PROJECT_NAME}</b></span> v{fileOps.current.getAppVersion()}</p>
 
           <h5 className='small-caps mt-4'>contact us</h5>
