70df37dfef564bfd1f326a7a48c6e4e8ca36fb94
Leon <7112003+leonlianght@users.noreply.github.com>
2021-03-24 14:55:04 +0800

    feat: add i18n support (#43)
    * feat: add i18n support
    
    * fix: move loading into loading screen

--- packages/ui-components/src/Screen/LoadingScreen.js
+++ packages/ui-components/src/Screen/LoadingScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import { t } from '@obsidians/i18n'
+
 import CenterScreen from './CenterScreen'
 
 export default function (props) {
-  const { text = 'Loading...' } = props
+  const { text = t('loading') } = props
   return (
     <CenterScreen>
       <i className='fas fa-spin fa-spinner mr-2' />{text}
