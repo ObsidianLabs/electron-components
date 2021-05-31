ab4fb954fd3686ba8423588bed8fe9dceff91147
Phil Li <qft.gtr@gmail.com>
2021-05-31 14:54:11 -0700

    update track middleware

--- packages/redux/src/middlewares.js
+++ packages/redux/src/middlewares.js
@@ -1,28 +1,42 @@
 import mixpanel from 'mixpanel-browser'
 import mapValues from 'lodash/mapValues'
+import platform from '@obsidians/platform'
 
+let mixpanelInitialized
 const track = store => next => action => {
+  if (process.env.REACT_APP_MIXPANEL_TOKEN) {
+    mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN)
+    mixpanelInitialized = true
+  }
+  if (!mixpanelInitialized) {
+    return next(action)
+  }
+  
   const { type, payload } = action
   const project = process.env.PROJECT
   if (type === 'SET_VERSION') {
-    if (process.env.REACT_APP_MIXPANEL_TOKEN) {
-      mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN)
+    if (platform.isDesktop) {
       const distinctId = store.getState().profile.get('distinctId') || mixpanel.get_distinct_id()
       mixpanel.identify(distinctId)
       payload.distinctId = distinctId
-      mixpanel.register({ project, version: payload.version })
-      mixpanel.people.set({ project, version: payload.version })
     }
+    mixpanel.register({ project, version: payload.version, platform: platform.type })
+    mixpanel.people.set({ project, version: payload.version, platform: platform.type })
+  } else if (type === 'UPDATE_PROFILE') {
+    const distinctId = `${payload.userId}:${project}`
+    mixpanel.identify(distinctId)
+    mixpanel.people.set({
+      $name: payload.username,
+      $avatar: payload.avatar,
+    })
+  } else if (type === 'CLEAR_USER_PROFILE') {
+    mixpanel.reset()
   }
-  try {
-    if (type.startsWith('persist/')) {
-      throw new Error()
-    }
+  if (!type.startsWith('persist/')) {
     mixpanel.track(`${project}/${type}`, typeof payload !== 'object' ? { value: payload } : payload)
-  } catch {}
+  }
 
-  let result = next(action)
-  return result
+  return next(action)
 }
 
 const middlewares = []
@@ -34,6 +48,7 @@ if (process.env.NODE_ENV === 'development') {
       stateTransformer: state => mapValues(state, s => (s.toJS ? s.toJS() : s))
     })
   )
+  middlewares.push(track)
 } else {
   middlewares.push(track)
 }
