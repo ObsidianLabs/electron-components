de039aa4287ab2fb846e84d83e263f6859a7caf7
Leon <leon390422914@gmail.com>
2021-06-16 11:47:19 +0800

    feat: add navbarCenter

--- packages/navbar/src/index.js
+++ packages/navbar/src/index.js
@@ -30,7 +30,7 @@ export default class Header extends PureComponent {
       />
     ))
   }
-  
+
   renderRightNavbar = links => {
     return links.map(link => (
       <NavLinkRight
@@ -69,7 +69,7 @@ export default class Header extends PureComponent {
   }
 
   render () {
-    const { profile, navbarLeft, navbarRight, extraLoggedInOptions } = this.props
+    const { profile, navbarLeft, navbarCenter, navbarRight, extraLoggedInOptions } = this.props
     const username = profile.get('username') || 'local'
 
     return (
@@ -77,6 +77,7 @@ export default class Header extends PureComponent {
         <Nav navbar className='navbar-left'>
           {this.renderLeftNavbar(navbarLeft)}
         </Nav>
+        {navbarCenter}
         <Nav navbar className='ml-auto navbar-nav-scroll navbar-right'>
           {this.renderRightNavbar(navbarRight)}
           <NavLink
