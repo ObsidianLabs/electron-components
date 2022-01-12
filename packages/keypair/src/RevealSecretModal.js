bd2251122c3bb1c2cb40f729153c6f6c622054a3
DennyWanye <37296472+DennyWanye@users.noreply.github.com>
2022-01-12 16:55:18 +0800

    fix:Keypair Manager | import, address format

--- packages/keypair/src/RevealSecretModal.js
+++ packages/keypair/src/RevealSecretModal.js
@@ -6,6 +6,8 @@ import {
 } from '@obsidians/ui-components'
 
 import keypairManager from './keypairManager'
+import { utils } from '@obsidians/eth-sdk'
+
 
 export default class RevealSecretModal extends PureComponent {
   constructor (props) {
@@ -41,7 +43,7 @@ export default class RevealSecretModal extends PureComponent {
             <Badge pill color='info' className='ml-1'>Address</Badge>
           </div>
           <div className='col-10 pl-0'>
-            <code className='user-select small'>{address}</code>
+            <code className='user-select small'>{utils.formatAddress(address) ? utils.formatAddress(address) : 'wrong address'}</code>
           </div>
         </div>
         <div className='row align-items-center'>
