6fecf2b1dd88a8099715af0c9bd166244907cde7
DennyWanye <37296472+DennyWanye@users.noreply.github.com>
2022-01-21 11:03:02 +0800

    feat: beautify tab scroll effect (#104)
    * feat(ui-components): add react-custom-scrollbars-2 lib, optimizing scroll function for tab
    
    * chore(Scrollbar):  delete code that is not being used

---
+++ packages/ui-components/src/Scrollbar/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars-2';
+
+export default class CustomScrollbar extends Component {
+
+    constructor(props, ...rest) {
+        super(props, ...rest);
+        this.state = { top: 0, left: 0 };
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.renderView = this.renderView.bind(this);
+        this.renderThumb = this.renderThumb.bind(this);
+        this.handleWheel = this.handleWheel.bind(this);
+        this.scrollSelf = React.createRef()
+    }
+
+    renderView({ style, ...props }) {
+        return (
+            <div
+                className="box"
+                style={{ ...style,  }}
+                {...props}/>
+        );
+    }
+
+    renderThumb({ style, ...props }) {
+        const thumbStyle = {
+            backgroundColor: 'rgb(255,255,255)',
+            borderRadius: '5px',
+        };
+        return (
+            <div
+                style={{ ...style, ...thumbStyle }}
+                {...props}/>
+        );
+    }
+
+    handleWheel(event) {
+        //todo: optimize scroll
+        event.preventDefault()
+        let scrollLeft = this.scrollSelf.current.getScrollLeft()
+        let wheelY = event.deltaY
+        this.scrollSelf.current.scrollLeft(scrollLeft + wheelY)
+    }
+
+    render() {
+        return (
+            <Scrollbars
+                ref={this.scrollSelf}
+                autoHide
+                autoHideTimeout={1000}
+                autoHideDuration={200}
+                renderView={this.renderView}
+                renderThumbHorizontal={this.renderThumb}
+                renderThumbVertical={this.renderThumb}
+                thumbSize={500}
+                thumbMinSize={500}
+                onWheel={this.handleWheel}
+                {...this.props}>
+            </Scrollbars>
+        );
+    }
+}
