8d45419ec8d2841cb3d343d8f5d655eb705ed4cb
北屿一木 <20736452+iNorthIsle@users.noreply.github.com>
2022-01-24 17:26:19 +0800

    perf: try to reduce bundle size (#105)
    * pref: use esmodule import
    
    * fix: aws module import
    
    * pref: update the deps
    
    * chore: update package jon
    
    * chore: update the provider env

--- packages/auth/src/providers/github.js
+++ packages/auth/src/providers/github.js
@@ -1,4 +1,4 @@
-import AWS from 'aws-sdk'
+import AWS from 'aws-sdk/global'
 import fileOps from '@obsidians/file-ops'
 import decode from 'jwt-decode'
 import { BuildService } from '@obsidians/ipc'
@@ -32,7 +32,7 @@ class GithubProvider extends BaseProvider {
       return 'local'
     }
     if(process.env.REACT_APP_ENV === 'test') {
-      return `${process.env.PROJECT}-test`
+      return 'eth-test'
     }
 
     return process.env.PROJECT
