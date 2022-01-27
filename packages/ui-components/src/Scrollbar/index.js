2ca1c0189285ca3f77b4ac349e99d3efa2ebfce8
DennyWanye <37296472+DennyWanye@users.noreply.github.com>
2022-01-28 06:44:34 +0800

    fix(Scrollbar): handle error with prevent default. (#107)
    * fix(Scrollbar): handle error with prevent default.
    
    * fix(Scroll): fix scroll bar.
    
    * perf(Scroll): imporove ui effets.

--- packages/ui-components/src/Scrollbar/index.js
+++ packages/ui-components/src/Scrollbar/index.js
@@ -6,34 +6,77 @@ export default class CustomScrollbar extends Component {
     constructor(props, ...rest) {
         super(props, ...rest);
         this.state = { top: 0, left: 0 };
-        this.renderView = this.renderView.bind(this);
-        this.renderThumb = this.renderThumb.bind(this);
-        this.handleWheel = this.handleWheel.bind(this);
         this.scrollSelf = React.createRef()
     }
 
-    renderView() {
+    renderView = ({ style, ...props }) => {
         return (
             <div
-                className="box"/>
+                className="box"
+                style={{ ...style,  }}
+                {...props}/>
         );
     }
 
-    renderThumb() {
+    renderThumbHorizontal = ({ style, ...props }) => {
         const thumbStyle = {
             backgroundColor: 'rgb(255,255,255)',
             borderRadius: '5px',
+            height: '63%',
+            opacity: '0.2',
+            position: 'absolute',
+            bottom: '0',
+            transition: 'all 0.5s ease-out',
+
         };
         return (
             <div
-                style={{ ...thumbStyle }}
-                />
+                style={{ ...style, ...thumbStyle }}
+                {...props}/>
         );
     }
 
-    handleWheel(event) {
+    renderTrackHorizontal = ({ style, ...props }) => {
+        const trackStyle = {
+            bottom: '0',
+            width: '100%',
+        };
+        return (
+            <div
+                className='trackHorizontal'
+                style={{ ...style, ...trackStyle }}
+                {...props}/>
+        );
+    }
+
+    renderTrackVertical = ({ style, ...props }) => {
+        const trackStyle = {
+            display: 'none',
+        };
+        return (
+            <div
+                className='trackVertical'
+                style={{ ...style, ...trackStyle }}
+                {...props}/>
+        );
+    }
+
+    renderThumbVertical = ({ style, ...props }) => {
+        const thumbStyle = {
+            backgroundColor: 'rgb(255,255,255)',
+            borderRadius: '5px',
+            height: '63%',
+            opacity: '0.2',
+        };
+        return (
+            <div
+                style={{ ...style, ...thumbStyle }}
+                {...props}/>
+        );
+    }
+
+    handleWheel = (event) => {
         //todo: optimize scroll
-        event.preventDefault()
         let scrollLeft = this.scrollSelf.current.getScrollLeft()
         let wheelY = event.deltaY
         this.scrollSelf.current.scrollLeft(scrollLeft + wheelY)
@@ -44,13 +87,15 @@ export default class CustomScrollbar extends Component {
             <Scrollbars
                 ref={this.scrollSelf}
                 autoHide
-                autoHideTimeout={1000}
+                autoHideTimeout={300}
                 autoHideDuration={200}
                 renderView={this.renderView}
-                renderThumbHorizontal={this.renderThumb}
-                renderThumbVertical={this.renderThumb}
-                thumbSize={500}
-                thumbMinSize={500}
+                renderThumbHorizontal={this.renderThumbHorizontal}
+                renderThumbVertical={this.renderThumbVertical}
+                renderTrackHorizontal={this.renderTrackHorizontal}
+                renderTrackVertical={this.renderTrackVertical}
+                thumbSize={282}
+                thumbMinSize={100}
                 onWheel={this.handleWheel}
                 {...this.props}>
             </Scrollbars>
