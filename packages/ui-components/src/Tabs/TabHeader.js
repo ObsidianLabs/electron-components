d2168fc290a05299a79c40bb80a3ba027c318892
DennyWanye <37296472+DennyWanye@users.noreply.github.com>
2022-02-16 16:15:06 +0800

    chore: delete annotation.

--- packages/ui-components/src/Tabs/TabHeader.js
+++ packages/ui-components/src/Tabs/TabHeader.js
@@ -127,11 +127,7 @@ export class TabHeaderItem extends PureComponent {
   componentDidMount() {
     const { connectDragPreview } = this.props;
     if (connectDragPreview) {
-      // Use empty image as a drag preview so browsers don't draw it
-      // and we can draw whatever we want on the custom drag layer instead.
       connectDragPreview(getEmptyImage(), {
-        // IE fallback: specify that we'd rather screenshot the node
-        // when it already knows it's being dragged so we can hide it with CSS.
         captureDraggingState: true,
       });
     }
