1dc9dffefdb2738942f5b97d988910dfdf2de41f
flowfire <ziyi.meng@obsidians.io>
2022-02-24 21:42:58 +0800

    feat: improve popup after rename file

--- packages/code-editor/src/CodeEditorCollection.js
+++ packages/code-editor/src/CodeEditorCollection.js
@@ -98,9 +98,9 @@ export default class CodeEditorCollection extends PureComponent {
     return tab => modelSessionManager.closeModelSession(tab.path)
   }
 
-  closeCurrentFile = tab => {
+  closeCurrentFile = () => {
     const { onCloseTab } = this.tabs.current
-    onCloseTab(tab)
+    onCloseTab(this.tabs.current)
   }
 
   // MARK: may can define a batch delete in the Tabs component
@@ -222,6 +222,9 @@ export default class CodeEditorCollection extends PureComponent {
       initialTab,
       readOnly,
     } = this.props
+
+    modelSessionManager.tabsRef = this.tabs
+    modelSessionManager.editorRef = this.editorContainer
 
     return (
       <div className={classnames('d-flex w-100 h-100 overflow-hidden', { bg2: this.tabs.current && this.tabs.current.state.tabs.length !== 1 })}>
