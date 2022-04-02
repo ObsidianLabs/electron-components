2771ab2782e7b36dabeff11efcc2b548f589b8da
yuan <38369170+dandansad@users.noreply.github.com>
2022-04-02 16:01:34 +0800

    fix: keypair operation then modify judgment conditions (#198)
    * fix: when login is required to operate the key pair then modify judgment conditions
    
    * fix: change the way data is obtained
    
    * fix: obtaining object data

--- packages/keypair/src/KeypairButton.js
+++ packages/keypair/src/KeypairButton.js
@@ -18,8 +18,10 @@ class KeypairButton extends PureComponent {
   }
 
   openModal = () => {
+    const profileState = redux.getState().profile
+    const profile = profileState.toJS()
     const providers = process.env.LOGIN_PROVIDERS ? process.env.LOGIN_PROVIDERS.split(',') : ['github']
-    if (!redux.store.getState().profile?.size) {
+    if (!profile.userId) {
       notification.error('Not Logged In', `Please <b>Login</b> before operation`)
       return setTimeout(() => {
         Auth.login(this.props.history, providers[0])
