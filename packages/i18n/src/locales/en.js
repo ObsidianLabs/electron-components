5a7e052c5005710de7ea972b44f6fdccaa5fe5d3
iNorthIsle <ndobox@outlook.com>
2022-05-10 10:57:48 +0800

    chore: add a example

--- packages/i18n/src/locales/en.js
+++ packages/i18n/src/locales/en.js
@@ -6,6 +6,11 @@ const translation = {
     message: '{{projectName}} is a graphic IDE for developing smart contracts on the {{chainName}} blockchain. To get started, please install the prerequisite tools for {{projectName}}.',
     start: 'Get Started',
     skip: 'Skip'
+  },
+  contract: {
+    deploy: {
+      fail: "Deploy Failed"
+    }
   }
 }
 
--- packages/i18n/src/locales/zh.js
+++ packages/i18n/src/locales/zh.js
@@ -6,6 +6,11 @@ const translation = {
     message: '{{projectName}} 是一个在 {{chainName}} 链上的集成开发环境，提供图形界面帮助开发者进行开发。请安装以下依赖工具来使用 {{projectName}}。',
     start: '开始使用',
     skip: '跳过'
+  },
+  contract: {
+    deploy: {
+      fail: "部署失败"
+    }
   }
 }
 
