29cab9b25d96d6f414260bb88a1309955a8a724b
Tab <d491405879@gmail.com>
2022-07-12 11:16:24 +0800

    feat: optimize notificaiton of createFileOrFolder (#306)

--- packages/workspace/src/ProjectManager/RemoteProjectManager.js
+++ packages/workspace/src/ProjectManager/RemoteProjectManager.js
@@ -197,29 +197,29 @@ export default class RemoteProjectManager extends BaseProjectManager {
     const filePath = this.path.join(basePath, name)
 
     if (!await this.hasFileOrFolder(filePath)) {
-      throw new Error(`File or Folder <b>${filePath}</b> already exists.`)
+      throw new Error(`File or Folder <b>${name}</b> already exists.`)
     }
 
     try {
       await this.ensureFile(filePath)
     } catch (e) {
-      throw new Error(`Fail to create the file <b>${this.pathInProject(filePath)}</b>.`)
+      throw new Error(`Fail to create the file <b>${name}</b>.`)
     }
 
     await this.refreshDirectory(basePath)
     return filePath
   }
 
-  async createNewFolder (basePath, name) {
+  async createNewFolder(basePath, name) {
     const folderPath = this.path.join(basePath, name)
     if (!await this.hasFileOrFolder(folderPath)) {
-      throw new Error(`File or Folder <b>${folderPath}</b> already exists.`)
+      throw new Error(`File or Folder <b>${name}</b> already exists.`)
     }
 
     try {
       await fileOps.web.fs.ensureDir(folderPath)
     } catch (e) {
-      throw new Error(`Fail to create the folder <b>${this.pathInProject(folderPath)}</b>.`)
+      throw new Error(`Fail to create the folder <b>${name}</b>.`)
     }
 
     await this.refreshDirectory(basePath)
