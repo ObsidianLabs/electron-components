439fce3081b90ce8517c0045891656641bd47ad3
flowfire <ziyi.meng@obsidians.io>
2022-08-24 17:41:08 +0800

    fix: update ast bar

--- packages/code-editor/src/MonacoEditor/MonacoEditorContainer.js
+++ packages/code-editor/src/MonacoEditor/MonacoEditorContainer.js
@@ -10,6 +10,7 @@ import {
   DropdownMenu,
   DropdownItem,
 } from '@obsidians/ui-components'
+import Tree from 'rc-tree'
 
 export default class MonacoEditorContainer extends PureComponent {
   static propTypes = {
@@ -25,7 +26,8 @@ export default class MonacoEditorContainer extends PureComponent {
     this.state = {
       initialized: false,
       loading: false,
-      modelSession: null
+      modelSession: null,
+      dropdownMenuLists: [],
     }
     this.customTab = React.createRef()
   }
@@ -88,10 +90,46 @@ export default class MonacoEditorContainer extends PureComponent {
     this.editor && this.editor.quickCommand()
   }
 
-  handleSetPosition(position){
+  handleSetPosition(_id, target){
+    const position = target.node.position
     if (modelSessionManager.monacoEditor) {
       modelSessionManager.monacoEditor.setPosition(position)
     }
+  }
+
+  renderSwitcherIcon ({ loading, expanded, data }) {
+
+    if (loading && data.children) {
+      return <span key='loading'><span className='fas fa-sm fa-pulse fa-spinner fa-fw' /></span>
+    }
+  
+    if (!data.children) {
+      return null
+    }
+  
+    return expanded ? (
+      <span key='switch-expanded'><span className='far fa-chevron-down fa-fw' /></span>
+    ) : (
+      <span key='switch-close'><span className='far fa-chevron-right fa-fw' /></span>
+    )
+  }
+
+  renderDropDownMenu(label){
+    const { modelSession } = this.state
+    let expandKeys = []
+   for(let item of modelSession.breadcrumb){
+    if (item.key === label.key) break
+    expandKeys.push(item.key)
+   }
+    return <Tree
+    treeData={modelSession._breadcrumbTree}
+    switcherIcon={(nodeProps) =>
+        this.renderSwitcherIcon(nodeProps)
+    }
+    defaultSelectedKeys={[label.key]}
+    defaultExpandedKeys={expandKeys}
+    onSelect={this.handleSetPosition.bind(this)}
+    ></Tree>
   }
 
   render () {
@@ -118,36 +156,28 @@ export default class MonacoEditorContainer extends PureComponent {
     }
 
     if (modelSession.breadcrumb) {
+      const { modelSession } = this.state
       const filename = modelSession.filePath.split('/').pop()
+      modelSession.breadcrumb[0].title = filename
+      modelSession._breadcrumbTree[0].title = filename
       breadcrumb = (
         <div className='topbar-breadcrumb-box'>
         <div className='px-2 border-bottom-black text-muted topbar-breadcrumb'>
-          <UncontrolledButtonDropdown direction='start' className={'scope-label'}>
-          <DropdownToggle
-            size='sm'
-            color='default'
-            className='rounded-0 text-muted px-2 text-nowrap text-overflow-dots'
-          >
-            {filename}
-          </DropdownToggle>
-          </UncontrolledButtonDropdown>
-          { modelSession.breadcrumb.map((item) => 
+          { modelSession.breadcrumb.map((item, index) => 
             <>
-            <div className='breadcrumb-delimiter'></div>
-            <UncontrolledButtonDropdown direction='start' className={'scope-label'}>
+            {index > 0 && <div className='breadcrumb-delimiter' key={`${item.key}-delimter`}>
+              <i className="cldr codicon codicon-chevron-right"></i>
+            </div>}
+            <UncontrolledButtonDropdown direction='down' className={'scope-label'} key={`${item.key}-dropdown`}>
             <DropdownToggle
               size='sm'
               color='default'
               className='rounded-0 text-muted px-2 text-nowrap text-overflow-dots'
             >
-              {item.current.name}
+              { item.title }
             </DropdownToggle>
             <DropdownMenu right className={'dropdown-menu-sm breadcrumb-dropdown'}>
-              {item.brothers.map(brother => 
-              <DropdownItem onClick={this.handleSetPosition.bind(this, brother.position)}>
-                <small>{brother.name}</small>
-              </DropdownItem>
-              )}
+              { this.renderDropDownMenu(item) }
             </DropdownMenu>
             </UncontrolledButtonDropdown>
             </>
