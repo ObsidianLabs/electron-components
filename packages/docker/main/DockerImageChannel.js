802e76c42915e3ef816ab34a3bc085cf96844c46
Ekpe_ <20736452+iNorthIsle@users.noreply.github.com>
2023-04-11 12:55:49 +0800

    update docker api url

--- packages/docker/main/DockerImageChannel.js
+++ packages/docker/main/DockerImageChannel.js
@@ -23,8 +23,8 @@ class DockerImageChannel extends IpcChannel {
   }
 
   async remoteVersions () {
-    const res = await this.fetch(`http://registry.hub.docker.com/v1/repositories/${this.imageName}/tags`)
-    return JSON.parse(res)
+    const res = await this.fetch(`https://registry.hub.docker.com/v2/repositories/${this.imageName}/tags`)
+    return JSON.parse(res).results
   }
 
   async any () {
